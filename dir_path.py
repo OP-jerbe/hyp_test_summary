994ac0248d3eb5276419e616132124d25c3705b7
OP-jerbe <j.erbe@oregon-physics.com>
2025-04-24 08:15:22 -0700

    Removes dir_path.py from .gitignore

---
+++ dir_path.py
@@ -0,0 +1,2 @@
+default = r'\\opdata2\Company\PRODUCTION FOLDER\Production History'
+DIR_PATH = r'\\opdata2\Company\PRODUCTION FOLDER\Production History\S-N 425 Cameca H201 18710\RMA 24045\Test Stand Data\All Test Data'
