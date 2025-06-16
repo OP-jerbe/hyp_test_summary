dabd0dae56fa22ab801a02caff2a7839f81d47aa
OP-jerbe <j.erbe@oregon-physics.com>
2025-06-16 13:49:19 -0700

    changes directory path

--- dir_path.py
+++ dir_path.py
@@ -1,2 +1,2 @@
 default = r'\\opdata2\Company\PRODUCTION FOLDER\Production History'
-DIR_PATH = r'\\opdata2\Company\PRODUCTION FOLDER\Production History\S-N 425 Cameca H201 18710\RMA 24045\Test Stand Data\All Test Data'
+DIR_PATH = r'\\opdata2\Company\PRODUCTION FOLDER\Production History\S-N 344 Hyperion 201 Cameca 18710-D\SN-344LRFC (RMA-24036)\Raw Test Data\needs to have beam sign flipped'
